b09b1ea2a64291620857c103454e8db84ddfa6bc
rakshithluckky <63341289+rakshithluckky@users.noreply.github.com>
2021-07-24 21:05:36 +0530

    Built-in Programs

---
+++ builtin1.py
@@ -0,0 +1,19 @@
+#1)len & 2)print
+a="This Is All"
+print(len(a))
+print()
+#3)lower & 4)upper
+print(a.lower())
+print()
+print(a.upper())
+print()
+#5)replace & 6)split
+print(a.replace("i","s"))
+print()
+print(a.split(" "))
+print()
+#7)strip
+a="   This is All   "
+print(a.strip())#removes white spaces at first and last
+print()
+print(a.strip("h"))#removes white spaces till the letter mentioned from last
