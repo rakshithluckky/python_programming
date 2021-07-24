5e8a62a0f59149bc1d528f20ee308d0985b57abb
rakshithluckky <63341289+rakshithluckky@users.noreply.github.com>
2021-07-24 21:11:47 +0530

    Built in Math functions

---
+++ mathbuiltin.py
@@ -0,0 +1,31 @@
+#Mathematical  functions
+#1)abs()   2)log()
+a=-45.17
+print("a=",a)
+print("absolutev value of a is ",abs(a))
+print()
+import math
+print("logarithmic value of  -a is ",math.log(-a))
+print()
+#3)ceil ()  4)floor()
+print("ceiling value of a is ",math.ceil(a))
+print()
+print("floor value of a is ",math.floor(a))
+print()
+#5)min()   6)max()
+a=(424,-4224,25353.235245,-2434343.535325235)
+print("minimum value of a is ",min(a))
+print()
+print("maximum value of a is ",max(a))
+print()
+#7)modf()   8)pow(a,b)
+a=-45.1723232
+b=5/3
+print("a=",a);print("b=",b)
+print(" float and  integer part of a is ",math.modf(a))
+print()
+print("power value of a to b is ",pow(a,b))
+print()
+#9)round(a,b)
+print("round off value of a to b points is ",round(a,2))
+
